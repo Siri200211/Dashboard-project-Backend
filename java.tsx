462675d46f5d44ba26f9b444aa6c236f765b1e6b
Sirimanne <it22325396@my.sliit.lk>
2025-11-12 22:51:06 +0530

    last push

---
+++ java.tsx
@@ -0,0 +1,19 @@
+export default function Java() {
+    return (
+        <div>
+
+        <h1>
+            Java
+        </h1>
+        </div>
+    )
+}
+export function sachi({para}) {
+    return (
+        <div>
+            <h1>
+                sachi
+            </h1>
+        </div>
+    )
+}
